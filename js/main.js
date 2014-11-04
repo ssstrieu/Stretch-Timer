ee5a390c526276941b119967505aeb3401b58bcc
ssstrieu <samanthastrieu@gmail.com>
2014-11-03 21:25:23 -0800

    load from previous stretch pro

---
+++ js/main.js
@@ -0,0 +1,63 @@
+alert('Stretch Break!');
+   
+
+function timeRefresh(timeoutPeriod) 
+{
+	setTimeout("location.reload(true);",timeoutPeriod);
+}
+
+var exercise={
+	{
+		name:,
+		area:forearm,
+		descrip:'';
+		reps:,
+		img:
+
+	},
+	wrist:{
+		name:,
+		descrip:'';
+		reps:,
+		img:
+	},
+	elbow{
+		name:,
+		descrip:'';
+		reps:,
+		img:
+	},
+
+}
+
+
+var tipDescription=['
+<li>
+	<ol>
+		<li> Start with your palms together in front of your chest just below your chin. </li>
+		<li> Slowly lower your hands toward your waistline, keeping your hands close to your stomach and your palms together, until you feel a mild to moderate stretch under your forearms. </li>
+		<li>Hold for at least 15 to 30 seconds. Repeat 2 to 4 times. </li>
+	</ol>
+</li>','
+<li>
+	<ol>
+		<li>Extend your arm in front of you with your palm up.</li>
+		<li>Bend your wrist, pointing your hand toward the floor.</li>
+		<li>With your other hand, gently bend your wrist farther until you feel a mild to moderate stretch in your forearm.</li>
+		<li>Hold for at least 15 to 30 seconds. Repeat 2 to 4 times.</li>
+
+	</ol>
+</li>'
+,'
+<li>
+	<ol>
+		<li> Start with your palms together in front of your chest just below your chin. </li>
+		<li> Slowly lower your hands toward your waistline, keeping your hands close to your stomach and your palms together, until you feel a mild to moderate stretch under your forearms. </li>
+		<li>Hold for at least 15 to 30 seconds. Repeat 2 to 4 times. </li>
+	</ol>
+</li>'
+]
+
+function randomTip (){
+	Math.random
+}
