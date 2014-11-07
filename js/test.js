69d3c3fdbdbc6c6d031560fc1e4fca5fd11474d0
ssstrieu <samanthastrieu@gmail.com>
2014-11-06 17:37:51 -0800

    11/06

--- js/test.js
+++ js/test.js
@@ -1,13 +1,20 @@
+debugger
+alert('hi this is test script.');
 
-	var formatTip=function(tipName,tipDescription,tipPicSrc){
-		$('h3').text(tipName);
-		$('.wrapper').append('img').attr('src',tipPicSrc);
-		for (var i = 0; i < tipDescription.length; i++) {
-			$('.wrapper').append('<li>').text(tipDescription[i]);
+//this works- displaying tip on page
+	var formatTip=function(title,descrArray,imgSrc){
+		$('h1').text(title);
+		$('.wrapper img').attr('src',imgSrc);
+		for (var i = 0; i < descrArray.length; i++) {
+			$('.wrapper').append('<li>');
+			$('.wrapper li').last().text(descrArray[i]);
 		}
-	}
+		$('.wrapper').append($('#newTip'));
+	}	
 
+	var testDescr=['one','two','three']
 
+//this doesn't work yet- cnnt to json file
 	var getJSONtip= $.getJSON( "tips.json", function(e) {
 		var tipNum=Math.floor(Math.random()*3)+1;
 		var tipName= tips[tipNum].name;
@@ -21,6 +28,9 @@
 		formatTip(tipName,tipDescription,tipPicSrc);
 	});
 
+		//test var
+	var putTip= formatTip('Name',testDescr,'img/02.jpg');
 
+	$('#newTip').on('click',putTip);
  
 
