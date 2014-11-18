e6e97551700371996f3066ede4142c067844e2e4
ssstrieu <samanthastrieu@gmail.com>
2014-11-17 20:57:03 -0800

    cleanup & added eye exercises

--- js/main.js
+++ js/main.js
@@ -1,47 +1,14 @@
-//debugger
-//alert('Stretch Break!');
+alert('Stretch Break!');
 
-//pass area parm from index page
-console.log(filterValue);
+setTimeout(function() {
+	window.location.reload();
+}, 30 * 60 * 1000); //30 minutes
 
-
-//this works- displaying tip on page
-	var formatTip=function(title,descrArray,imgSrc,source){
-		$('.tipName').text(title);
-		$('.tip-wrapper img').attr('src',imgSrc);
-		$('.tip-wrapper a').attr('href',source);
-		$('.tip-wrapper a').text(source);
-
-		for (var i = 0; i < descrArray.length; i++) {
-			//console.log('starting desciprtion');
-			$('.tipIntructions').append('<li>');
-			$('.tipIntructions li').last().text(descrArray[i]);
-		}
-		$('.tip-wrapper').append($('#newTip'));
-	}	
-
-
-//trying to filter types of stretches, passes parm
-$('#timerStart').on('click', function(ev){
-	ev.preventDefault();
-
-	filterValue=$('#focusArea').val();
-	var url = 'stretch.html'+'#'+filterValue;
-	console.log("The filterValue is "+filterValue);
-	window.location = url;
-});
-
-//redefine filtervalue on second page by passing parm from index
-var filterValue='all'; //give default
-var filterListOn=location.hash.split('#')[1];
-console.log('filterValue is:',filterValue,'| filterListOn : ',filterListOn);
 var filteredList=[];
 var neckList=[];
 var backList=[];
 var forearmList=[];
-var tipObject={};
-
-
+var eyeList=[];
 
 //filters all exercises into groupArrays
 	$.ajax({
@@ -55,22 +22,51 @@ var tipObject={};
 					neckList.push(response[i]);
 					console.log('necklist done');
 				}
-				if(response[i].area.toLowerCase()=='forearm'){
+				else if(response[i].area.toLowerCase()=='forearm'){
 					forearmList.push(response[i]);
 					console.log('forearm list done');
 				}
-				if(response[i].area.toLowerCase()=='back'){
+				else if(response[i].area.toLowerCase()=='back'){
 					backList.push(response[i]);
 					console.log('back list done');
+				}
+					else if(response[i].area.toLowerCase()=='eye'){
+					eyeList.push(response[i]);
+					console.log('eye list done');
 				}
 			}
 		}
 	});
 
+//passes user's area selection to stretch page
+$('#timerStart').on('click', function(ev){
+	ev.preventDefault();
 
-//now present list according to user selection
-//connect to json
-//parms: title,descrArray,imgSrc,source
+	var filterValue=$('#focusArea').val();
+	var url = 'stretch.html'+'#'+filterValue;
+	console.log("The filterValue is "+filterValue);
+	window.location = url;
+});
+//passing user parm in url
+var filterListOn=location.hash.split('#')[1];
+
+//formats tip on page
+	var formatTip=function(title,descrArray,imgSrc,source){
+		$('.tipName').text(title);
+		$('.tip-wrapper img').attr('src',imgSrc);
+		$('.tip-wrapper a').attr('href',source);
+		$('.tip-wrapper a').text(source);
+
+		for (var i = 0; i < descrArray.length; i++) {
+			//console.log('starting desciprtion');
+			$('.tipIntructions').append('<li>');
+			$('.tipIntructions li').last().text(descrArray[i]);
+		}
+		$('.tip-wrapper').append($('#newTip'));
+	}
+
+
+//select the filtered list according to user selection
 	$.ajax({
 		url: 'js/tips.json',
 		type:"GET",
@@ -80,44 +76,34 @@ var tipObject={};
 			if (filterListOn=='neck') {
 			filteredList=neckList;
 			getRandomTip();
-			}
-			if (filterListOn=='back') {
+			} else if (filterListOn=='back') {
 			filteredList=backList;
 			getRandomTip();
-			}
-			if (filterListOn=='forearm') {
+			} else if (filterListOn=='forearm') {
 			filteredList=forearmList;
 			getRandomTip();
-			}
-			else{
+			} else if (filterListOn=='eye') {
+			filteredList=eyeList;
+			getRandomTip();
+			} else {
 				for (var i = 0; i < response.length; i++) {
 					filteredList.push(response[i]);
-					getRandomTip();
-					console.log(response[i]);
 				}
-				
+				getRandomTip();
 			}
 		}
 	});
 
-
-var tipObject={};
-var n=0; //default
-
+//chooses a random tip and calls format fxn with it
 	function getRandomTip(){
-		n=(Math.floor((Math.random())*filteredList.length));
-		tipObject=(filteredList[n]);
+		var n=(Math.floor((Math.random())*filteredList.length));
+		var tipObject=(filteredList[n]);
 		formatTip(tipObject.name,tipObject.instructions,tipObject.img,tipObject.source);
 	}
 
-	//formatTip(tipObject.name,tipObject.instructions,tipObject.img,tipObject.source);
-	//formatTip('name of stretch',[1,2,3],'img/02.jpg','www.google.com');
-	
-
-
-
-
-
+$('#newTip').on('click',function(){
+	window.location.reload();
+})
 
 
 
